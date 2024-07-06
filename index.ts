e3685e9b93058b2c9db90dfc76f2ccc2ad01facd
Nilay Pophalkar <nilaypophalkar@gmail.com>
2024-07-06 14:39:26 +0530

    updated / home path response

--- index.ts
+++ index.ts
@@ -19,12 +19,12 @@ const next_posts_load_state_schema = z.object({
 app.get("/", async (req: Request, res: Response) => {
   const initialPosts: Post[] = posts.slice(0, 10);
 
-  return res.status(200).json({
+  return res.status(200).render("main", { data: { posts: initialPosts } });
+
+  res.status(200).json({
     status: "success",
     message: "Welcome to the world of server side rendering",
   });
-
-  res.status(200).render("main", { data: { posts: initialPosts } });
 });
 
 app.get("/loadNext", async (req: Request, res: Response) => {
